6c8fc1e80e5b1cfe516a109e951ec9807bd3d56d
dfriedenbach <danielfriedenbach@gmail.com>
2015-07-22 15:26:06 -0700

    Serving index.html for GET requests to /

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,39 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
+var actions = {
+  'GET': function(req, res) {
+    httpHelpers.serveAssets(res, './public/index.html', function(error, content) {
+      if(error) {
+        res.writeHead(500);
+        res.end();
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(content);
+      }
+    });
+  },
+
+  'POST': function(req, res) {
+    console.log('post');
+    res.writeHead(201);
+    res.end();
+  },
+
+  'OPTIONS': function(req, res) {
+
+  }
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var action = actions[req.method];
+  if(action) {
+    action(req, res)
+  } else {
+    res.writeHead(405);
+    res.end('Method not allowed');
+  }
+
 };
